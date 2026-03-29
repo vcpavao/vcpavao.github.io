7481845405a2b8af31ef3be08512f5acb2d4af38
Victor Pavao <vcpavao@gmail.com>
2026-03-29 13:04:50 -0400

    Copy tweaks to project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,7 +17,7 @@ const projects = [
     body:
       "I explored podman as an open-source software for containerizing the components of a full-stack " +
       "geospatial web application. I also researched Red Hat OpenShift as a cloud-based platform for hosting the containers, " +
-      "but my computer unable to run OpenShift clusters locally, and ROSA (Red Hat OpenShift on AWS) did not have a " +
+      "but my computer was unable to run OpenShift clusters locally, and ROSA (Red Hat OpenShift on AWS) did not have a " +
       "free option for students. I discuss these roadblocks in my presentation, which was my term project for the " +
       "Spatial Databases course.",
     link: "View the slides here",
@@ -29,7 +29,7 @@ const projects = [
       "I built a full-stack web application for uploading GPX trail files and displaying them on a map " +
       "in the browser. The trails were converted from GPX and stored in a PostGIS database hosted on AWS (AWS RDS). " +
       "The trails were queried by server-side API calls written in Node/Express.js. " +
-      "This project exposed me to the multiple working parts of a geospatial application, including the GDAL toolkit.",
+      "This project exposed me to the multiple working parts of a geospatial web application, including the GDAL toolkit.",
   },
   {
     title: "Estimating domestic wells in Massachusetts",
@@ -38,7 +38,7 @@ const projects = [
       "Jupyter Notebook script and a Python toolbox for estimating which tax parcels in the state contain active, " +
       "domestic wells. The script and toolbox follow a detailed methodology based on tax parcel attributes such as " +
       "property use code, building style, and proximity to a Public Water System. I also created and am helping " +
-      "administer the ArcGIS Online map for displaying the relevant data for project stakeholders.",
+      "administer the ArcGIS Dashboard and other ArcGIS Online maps for displaying the relevant data to project stakeholders.",
     link: "View my scripts here",
     linkUrl:
       "https://github.com/vcpavao/arcpy-boilerplate/blob/main/massdep_wells_estimates/Wells_Estimates.ipynb",
